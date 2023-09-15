1bb1330c8329160e055cf1a6e9effa346e61c1be
KayqueS <kayque.sanmartin65@gmail.com>
2023-09-15 01:03:52 -0300

    Adionado função para mostrar a lista de bandas registradas utilizando o loop for.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 // Declarando uma lista
-List<string> listaDasBandas = new List<string>();
+List<string> listaDasBandas = new List<string> {"Linkin Park", "Imagine Dragons", "Twice"};
 
 void ExibirLogo() {
     //@ -> Verbatim Literal
@@ -33,7 +33,7 @@ void ExibirOpcoesDoMenu() {
     switch (opcaoEscolhidaNumerica) {
         case 1: RegistrarBanda();
             break;
-        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 2: MostrarBandasRegistradas();
             break;
         case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
@@ -48,12 +48,28 @@ void ExibirOpcoesDoMenu() {
 
 void RegistrarBanda() {
     Console.Clear();
+    Console.WriteLine("**********************");
     Console.WriteLine("Registro de Bandas");
+    Console.WriteLine("**********************\n");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
     listaDasBandas.Add(nomeDaBanda);
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarBandasRegistradas() {
+    Console.Clear();
+    Console.WriteLine("***************************************");
+    Console.WriteLine("Exibindo todas as Bandas Registradas");
+    Console.WriteLine("***************************************\n");
+    for (int i = 0; i < listaDasBandas.Count; i++) {
+        Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
 }
