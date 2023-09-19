83f01d19ea1d3530d4508c774fcf0d081b22a0c8
KayqueS <kayque.sanmartin65@gmail.com>
2023-09-18 23:31:10 -0300

    Nova função adicionada, permitindo a avaliação de uma banda, refatorando o código, utilizando Dicionário e Listas.

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 // Declarando uma lista
-List<string> listaDasBandas = new List<string> {"Linkin Park", "Imagine Dragons", "Twice"};
+//List<string> listaDasBandas = new List<string> {"Linkin Park", "Imagine Dragons", "Twice"};
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6});
+bandasRegistradas.Add("Beatles", new List<int>());
 
 void ExibirLogo() {
     //@ -> Verbatim Literal
@@ -35,7 +38,7 @@ void ExibirOpcoesDoMenu() {
             break;
         case 2: MostrarBandasRegistradas();
             break;
-        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 3: AvaliarUmaBanda();
             break;
         case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
@@ -48,12 +51,10 @@ void ExibirOpcoesDoMenu() {
 
 void RegistrarBanda() {
     Console.Clear();
-    Console.WriteLine("**********************");
-    Console.WriteLine("Registro de Bandas");
-    Console.WriteLine("**********************\n");
+    ExibirTituloDaOpcao("Registro das Bandas");
     Console.Write("Digite o nome da banda que deseja registrar: ");
     string nomeDaBanda = Console.ReadLine()!;
-    listaDasBandas.Add(nomeDaBanda);
+    bandasRegistradas.Add(nomeDaBanda, new List<int> { });
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
     Thread.Sleep(2000);
     Console.Clear();
@@ -62,19 +63,52 @@ void RegistrarBanda() {
 
 void MostrarBandasRegistradas() {
     Console.Clear();
-    Console.WriteLine("*************************************");
-    Console.WriteLine("Exibindo todas as Bandas Registradas");
-    Console.WriteLine("*************************************\n");
+    ExibirTituloDaOpcao("Exibindo todas as Bandas Registradas");
     //for (int i = 0; i < listaDasBandas.Count; i++) {
     //    Console.WriteLine($"Banda: {listaDasBandas[i]}");
     //}
-    foreach (string banda in listaDasBandas) {
+    foreach (string banda in bandasRegistradas.Keys) {
         Console.WriteLine($"Banda: {banda}");
     }
     Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
     Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
+}
+
+void ExibirTituloDaOpcao(string titulo) {
+    int quantidadeDeLetras = titulo.Length;
+    string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
+    Console.WriteLine(asteriscos);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asteriscos + "\n");
+}
+
+void AvaliarUmaBanda() {
+    // Digita qual banda deseja avaliar
+    // Se a banda existir no dicionário >> atribuir uma nota
+    // Se não, volta ao menu principal
+    // Melhoria, permitir somente notas de 0 a 10
+    // Invés de escrever o nome da banda, mostrar as opções e o usuário escolher a banda que deseja avaliar
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar Banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda)) {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}");
+        Thread.Sleep(4000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    } else {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal.");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
 }
 
 ExibirOpcoesDoMenu();
